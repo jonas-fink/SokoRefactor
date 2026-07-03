054cfbd0cba25fed3fb150c29788c28e0656970c
clizzix <godsilla1993@gmail.com>
2026-07-03 19:47:58 +0200

    initial scaffold: MERN monorepo

---
+++ soko-backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errorHandler } from '#middlewares';
+
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(errorHandler);
+
+export default app;
---
+++ soko-client/src/pages/LandingPage.tsx
@@ -0,0 +1,5 @@
+const LandingPage = () => {
+    return <div>Hello World!</div>;
+};
+
+export default LandingPage;

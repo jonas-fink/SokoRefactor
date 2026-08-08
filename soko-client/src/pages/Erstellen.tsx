d61ca3d9fa799e33478ecb0fb6641aa9b7032c1e
clizzix <godsilla1993@gmail.com>
2026-08-08 12:33:05 +0200

    optimization of the Erstellen Page styling

--- soko-client/src/pages/Erstellen.tsx
+++ soko-client/src/pages/Erstellen.tsx
@@ -16,22 +16,22 @@ const Erstellen = () => {
     const choice = isAdmin ? userChoice : 'Event erstellen';
 
     return (
-        <div className="flex flex-col gap-8 w-full max-w-4xl px-4 min-h-screen mx-auto mb-8">
+        <div className="flex flex-col gap-6 md:max-w-6xl md:p-8 mx-auto">
             {choice === null && (
                 <div className="flex gap-4">
                     <button
-                        className="card bg-primary flex h-50 flex-col justify-center items-center w-1/2 shadow-card p-8 cursor-pointer font-display text-4xl text-ink self-center gap-4"
+                        className="card events cursor-pointer font-bold text-3xl"
                         onClick={() => setUserChoice('Event erstellen')}
                     >
                         Event erstellen
                         <AiOutlinePlusCircle
                             size={48}
-                            className="hover:text-primary-ink transition-all duration-300 ease-in"
+                            className="hover:text-error transition-all duration-300 ease-in"
                         />
                     </button>
 
                     <button
-                        className="card bg-warning flex h-50 flex-col justify-center items-center w-1/2 shadow-card p-8 cursor-pointer font-display text-4xl text-primary-ink self-center gap-4"
+                        className="card beratung cursor-pointer font-bold text-3xl"
                         onClick={() =>
                             setUserChoice('Beratungsangebot erstellen')
                         }
